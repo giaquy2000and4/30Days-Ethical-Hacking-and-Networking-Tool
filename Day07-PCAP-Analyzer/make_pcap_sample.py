e015c1d0b2cf89165c102a836c9c6676f0842dfc
Pham Gia Quy <phamgiaquy2004@gmail.com>
2025-08-19 11:31:45 +0700

    fix commit modified

--- Day07-PCAP-Analyzer/make_pcap_sample.py
+++ Day07-PCAP-Analyzer/make_pcap_sample.py
@@ -3,13 +3,13 @@ from scapy.all import Ether, IP, TCP, UDP, DNS, DNSQR, Raw, wrpcap
 packets = []
 
 # DNS Query
-dns_pkt = Ether()/IP(src="192.168.0.7", dst="8.8.8.8")/UDP(sport=12345, dport=53)/DNS(rd=1, qd=DNSQR(qname="test.local"))
+dns_pkt = Ether()/IP(src="192.168.0.7", dst="8.8.8.8")/UDP(sport=12345, dport=53)/DNS(rd=1, qd=DNSQR(qname="Insta-Hunter-Img.local"))
 packets.append(dns_pkt)
 
 # HTTP GET
 http_get = (
     "GET / HTTP/1.1\r\n"
-    "Host: test.local\r\n"
+    "Host: Insta-Hunter-Img.local\r\n"
     "User-Agent: curl/7.68.0\r\n"
     "Accept: */*\r\n\r\n"
 )
@@ -20,7 +20,7 @@ packets.append(
 # HTTP POST (login)
 http_post = (
     "POST /login HTTP/1.1\r\n"
-    "Host: test.local\r\n"
+    "Host: Insta-Hunter-Img.local\r\n"
     "Content-Type: application/x-www-form-urlencoded\r\n"
     "Content-Length: 29\r\n\r\n"
     "username=admin&password=123"
@@ -32,10 +32,10 @@ packets.append(
 # HTTP POST (file upload)
 multipart = (
     "POST /upload HTTP/1.1\r\n"
-    "Host: test.local\r\n"
+    "Host: Insta-Hunter-Img.local\r\n"
     "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary\r\n\r\n"
     "------WebKitFormBoundary\r\n"
-    "Content-Disposition: form-data; name=\"file\"; filename=\"test.txt\"\r\n"
+    "Content-Disposition: form-data; name=\"file\"; filename=\"Insta-Hunter-Img.txt\"\r\n"
     "Content-Type: text/plain\r\n\r\n"
     "This is the content of the uploaded file.\r\n"
     "------WebKitFormBoundary--\r\n"
